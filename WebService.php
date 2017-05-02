afb2ac3e467f88e64d0094f9f279ca94029635a3
lucas.tomasi <lcstomasi@gmail.com>
2017-05-01 21:34:27 -0300

    Revert "Add envio de email"
    This reverts commit 05367605bd4262ed2520d0007d9ed1c2bd3430cc.

--- WebService.php
+++ WebService.php
@@ -1,13 +1,6 @@
 <?php 
 $class = $_REQUEST['class'];
 $method = $_REQUEST['method'];
-
-if( !$class OR ! $method OR !file_exists("app/services/{$class}.class.php") )
-{
-	die( "Permission denied!" );
-}
-
-
 require_once 'init.php';
 require_once "app/services/{$class}.class.php";
 
