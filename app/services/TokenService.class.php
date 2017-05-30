bc55fcea11fb5d75505d491271fde300ddcd73be
Lucas Tomasi <lcstomasi@gmail.com>
2017-05-30 17:38:21 -0300

    Adicionado remoção de tokens antigos

--- app/services/TokenService.class.php
+++ app/services/TokenService.class.php
@@ -14,10 +14,17 @@ class TokenService
 			try
 			{
 				TTransaction::open('eventtus');
+				
+				$criteria = new TCriteria;
+				$criteria->add(new TFilter('email', '=',$param['email']);
+				$repository = new TRepository('Token');
+				$repository->delete($criteria);
+				
 				$token = new Token();
 				$token->email = $param['email'];
 				$token->token = $param['token'];
 				$token->store();
+					       
 				TTransaction::close();
 			}
 			catch( Exception $e )
