0e2c4bcc256fd84e55a19c751264a520e086b8fa
lucas.tomasi <lcstomasi@gmail.com>
2017-06-01 20:24:20 -0300

    Add function para buscar medias de avaliações de atividades

--- app/services/EvaluationService.class.php
+++ app/services/EvaluationService.class.php
@@ -36,5 +36,49 @@ class EvaluationService
 			echo json_encode($objResponse);
 		}
 	}
+
+	public function getMedia($params)
+	{
+		$objResponse = new stdClass;
+		if( isset( $params['activity_id'] ) )
+		{
+			try
+			{
+				TTransaction::open('eventtus');
+				
+				$conn = TTransaction::get();
+				
+				$sth = $conn->prepare('SELECT count(id) as count, avg(stars) as avg from evaluations WHERE ref_activity = ?'); 
+				$sth->execute([$params['activity_id']]);
+            	$results = $sth->fetchAll();
+				
+				$result = new stdClass;
+				$result->count = $results[0]['count'];
+				$result->avg   = $results[0]['avg'];
+
+				$objResponse->data    = $result;
+				$objResponse->status  = 'success';
+				
+				TTransaction::close();
+			}
+			catch(Exception $e)
+			{
+				$objResponse->status  = 'error';
+				$objResponse->message = $e->getMessage();
+				TTransaction::rollback();
+			}
+
+			finally
+			{
+				echo json_encode( $objResponse );
+			}
+		}
+		else
+		{
+			$objResponse->status  = 'error';
+			$objResponse->message = 'faltam parametros';
+			echo json_encode( $objResponse );
+		}
+	}
 }
 ?>	
