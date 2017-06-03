15b3174b9561ef437dfc40c14db18e5cebcd3146
ArturTomasi <tomasi.artur@gmail.com>
2017-06-03 17:32:05 -0300

    Criado webservices para finalizar pesquisa e verificar se ha disponivel

--- app/services/SurveyService.class.php
+++ app/services/SurveyService.class.php
@@ -1,10 +1,128 @@
-<?php 
+<?php
 /**
 * @author lucas.tomasi
 */
 class SurveyService
 {
-	
+
+	function store( $params )
+	{
+			$objResponse = new stdClass;
+
+			if( isset( $params[ 'activity_id' ] ) )
+			{
+					try
+					{
+							TTransaction::open('eventtus');
+
+							$repository = new TRepository('SurveyAnswer');
+
+							$activity = $params[ 'activity_id' ];
+							$email 		= $params[ 'email' ];
+
+							$answers = json_decode( $params[ 'answers' ] );
+
+							if ( $answers )
+							{
+									foreach ( $answers as $answer )
+									{
+										$result = new SurveyAnswer();
+
+										$result->survey_options_id = $answer['option'];
+										$result->email = $email;
+										$result->survey_id = $answer['question'];
+
+										$result->store();
+									}
+							}
+
+							$objResponse->status  = 'success';
+							$objResponse->message = 'ok';
+
+							TTransaction::close();
+					}
+
+					catch ( Exception $e )
+					{
+						$objResponse->status  = 'error';
+						$objResponse->message = $e->getMessage();
+						TTransaction::rollback();
+					}
+
+					finally
+					{
+							echo json_encode( $objResponse );
+					}
+			}
+
+			else
+		 	{
+				 $objResponse->status  = 'error';
+				 $objResponse->message = 'faltam parametros';
+				 echo json_encode( $objResponse );
+			}
+	}
+
+	function hasSurvey( $params )
+	{
+			$objResponse = new stdClass;
+
+			if( isset($params['activity_id']) )
+			{
+					try
+					{
+							TTransaction::open('eventtus');
+
+							$objResponse->hasSurvey = true;
+
+							$repository = new TRepository('Survey');
+							$criteria   = new TCriteria;
+							$criteria->add( new TFilter('activity_id', '=', $params['activity_id']) );
+
+							$surveys = $repository->load($criteria);
+
+							$repository = new TRepository('SurveyAnswer');
+							$criteria   = new TCriteria;
+							$criteria->add( new TFilter('email', '=', $params['email']) );
+
+							foreach ($surveys as $survey )
+							{
+									$criteria->add( new TFilter('survey_id', '=', $survey->id );
+
+									$answers = $repository->load($criteria);
+
+									if ( $answers )
+									{
+											$objResponse->hasSurvey = false;
+											$objResponse->status = 'success';
+											break;
+									}
+							}
+
+							TTransaction::close();
+					}
+
+					catch (Exception $e)
+					{
+						$objResponse->status  = 'error';
+						$objResponse->message = $e->getMessage();
+						TTransaction::rollback();
+					}
+
+					finally
+					{
+						echo json_encode( $objResponse );
+					}
+			}
+
+			else
+			{
+				$objResponse->status  = 'error';
+				$objResponse->message = 'faltam parametros';
+				echo json_encode( $objResponse );
+			}
+	}
+
 	function getSurveys($params)
 	{
 		$objResponse = new stdClass;
@@ -13,14 +131,14 @@ class SurveyService
 			try
 			{
 				TTransaction::open('eventtus');
-				
+
 				$repository = new TRepository('Survey');
 				$criteria   = new TCriteria;
 				$criteria->add( new TFilter('activity_id', '=', $params['activity_id']) );
 				$surveys = $repository->load($criteria);
 
 				$activity = new Activity($params['activity_id']);
-				
+
 				$result = new stdClass;
 				$result->id 		 = $activity->id;
 				$result->description = $activity->name;
@@ -38,18 +156,18 @@ class SurveyService
 						$repository = new TRepository('SurveyOption');
 						$criteria   = new TCriteria;
 						$criteria->add( new TFilter('survey_id', '=', $value->id) );
-						$objOptions = $repository->load($criteria);	
+						$objOptions = $repository->load($criteria);
 
 						if( $objOptions )
 						{
 							foreach( $objOptions as $objOption )
 							{
 								$opt = new Option($objOption->option_id);
-								
+
 								$op  = new stdClass;
 								$op->id          = $opt->id;
 								$op->description = $opt->description;
-								
+
 								$option->options[] = $op;
 							}
 						}
@@ -62,14 +180,14 @@ class SurveyService
 				}
 
 				$objResponse->status = 'success';
-				
+
 				TTransaction::close();
 			}
 			catch (Exception $e)
 			{
 				$objResponse->status  = 'error';
 				$objResponse->message = $e->getMessage();
-				TTransaction::rollback();	
+				TTransaction::rollback();
 			}
 
 			finally
